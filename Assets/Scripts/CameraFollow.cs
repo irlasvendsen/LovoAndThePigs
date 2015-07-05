77d7ecc0470e3f7adfd96f96766d7d54d8f22865
irlasvendsen <josephherendeen@MacBook-Pro-de-Joseph-Herendeen.local>
2015-07-05 12:08:07 -0300

    adicionado movimento de camera
    Adicionado o movimento da camera junto ao player.

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	public Transform target;
+	Camera mycam;
+	public float m_speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		mycam = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		mycam.orthographicSize = (Screen.height / 100f) / 4f;
+		if (target) {
+			transform.position = Vector3.Lerp(transform.position, target.position, m_speed) + new Vector3(0, 0, -10);
+		}
+	}
+}
