51f07d75812638ccc22beeabad1a16e84be5a9df
Marcos Silva <marcos.silva79@gmail.com>
2015-07-07 14:13:43 -0300

    Add PlayerBlowing method/ LovoBlowing Animation

---
+++ LovoAndThePigs_teste/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	//Player movement control 
+	Rigidbody2D rbody;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		rbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 movement_vector = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+	
+		if( movement_vector != Vector2.zero)
+		{
+			anim.SetBool  ("IsWalking",true);
+			anim.SetFloat  ("Input_X",movement_vector.x);
+			anim.SetFloat ("Input_Y",movement_vector.y);
+		}
+		else
+		{
+			anim.SetBool  ("IsWalking", false);
+		}
+
+		rbody.MovePosition(rbody.position + movement_vector*Time.deltaTime);
+
+
+
+	}
+}
